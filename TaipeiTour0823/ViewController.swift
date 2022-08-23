c1ee242120238fb1b543d758bd97ed2af1491058
lzrong0203 <lzrong0203@gmail.com>
2022-08-23 14:21:45 +0800

    Initial Commit

---
+++ TaipeiTour0823/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaipeiTour0823
+//
+//  Created by Steve Lai on 2022/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
